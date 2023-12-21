88a5db88db3a32b11a710290d40660fd72a07612
moki <morozov.kirill.moki@gmail.com>
2023-12-21 21:31:21 +0400

    test(json/JSONRefResolver): resolve json refs

---
+++ src/json/__fixtures__/index.ts
@@ -0,0 +1,5 @@
+const withRefs = require('./with-refs.json');
+const withoutRefs = require('./without-refs.json');
+
+export {withRefs, withoutRefs};
+export default {withRefs, withoutRefs};
---
+++ src/json/json-ref-resolver.test.ts
@@ -0,0 +1,18 @@
+import {JSONRefResolver} from './json-ref-resolver';
+import {withRefs, withoutRefs} from './__fixtures__';
+
+describe('json ref resolver', () => {
+    it('resolves refs', async () => {
+        const jsonRefResolver = new JSONRefResolver({resolveCircular: true});
+
+        const actual = await jsonRefResolver.resolve(withRefs);
+        expect(actual).toMatchSnapshot();
+    });
+
+    it('works on objects without refs', async () => {
+        const jsonRefResolver = new JSONRefResolver({resolveCircular: true});
+
+        const actual = await jsonRefResolver.resolve(withoutRefs);
+        expect(actual).toMatchSnapshot();
+    });
+});
