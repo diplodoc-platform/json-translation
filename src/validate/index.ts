afdc33d2a8886a5904f38526f09d10bef0f2f5dd
moki <morozov.kirill.moki@gmail.com>
2023-12-21 17:17:34 +0400

    feat(validate): isPlainObject, isNonEmptyObject, isIntStr

---
+++ src/validate/index.ts
@@ -0,0 +1,14 @@
+function isPlainObject(val: unknown) {
+    return val != null && Object.prototype.toString.call(val) === '[object Object]';
+}
+
+function isNonEmptyObject(val: object) {
+    return Boolean(Object.keys(val).length);
+}
+
+function isIntStr(str: string) {
+    return !isNaN(parseInt(str, 10));
+}
+
+export {isPlainObject, isNonEmptyObject, isIntStr};
+export default {isPlainObject, isNonEmptyObject, isIntStr};
