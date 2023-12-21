f213707f85af399c74543a8aef4e5edc95590fdf
moki <morozov.kirill.moki@gmail.com>
2023-12-21 17:19:19 +0400

    test(api/compose): validate parameters

---
+++ src/api/compose.test.ts
@@ -0,0 +1,44 @@
+import {compose} from './compose';
+import {JSONTranslationComposeParameters} from './types';
+
+describe('compose: parameters validation', () => {
+    it('works with valid parameters', async () => {
+        await compose({schema: {$schema: 'b'}, skeleton: {'1': 'a'}, xliff: 'a'});
+    });
+
+    it('throws on invalid schema', async () => {
+        const parameters = {
+            schema: null,
+            skeleton: {'1': 'a'},
+            xliff: 'a',
+        } as unknown as JSONTranslationComposeParameters;
+        await expect(compose(parameters)).rejects.toThrow();
+
+        parameters.schema = {};
+        await expect(compose(parameters)).rejects.toThrow();
+    });
+
+    it('throws on invalid skeleton', async () => {
+        const parameters = {
+            schema: {$schema: 'a'},
+            xliff: 'a',
+            skeleton: null,
+        } as unknown as JSONTranslationComposeParameters;
+        await expect(compose(parameters)).rejects.toThrow();
+
+        parameters.skeleton = {};
+        await expect(compose(parameters)).rejects.toThrow();
+    });
+
+    it('throws on invalid xliff', async () => {
+        const parameters = {
+            skeleton: {'1': 'a'},
+            schema: {$schema: 'a'},
+            xliff: '',
+        } as unknown as JSONTranslationComposeParameters;
+        await expect(compose(parameters)).rejects.toThrow();
+
+        parameters.xliff = null as unknown as string;
+        await expect(compose(parameters)).rejects.toThrow();
+    });
+});
